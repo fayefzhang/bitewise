d4a82fe9ec26736690cf0c9f0906a858398f93d9
sanyashetty <shettysanya@gmail.com>
2024-11-26 17:01:16 -0500

    added CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 import axios from 'axios';
 import routes from './routes/APIRoutes';
+import cors from "cors";
+
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
 app.use(express.json());
-
 app.use("/api", routes);
 
 
