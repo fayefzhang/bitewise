ef48fe6159bf384668375aa9407f5ef48df71414
Karen Li <kli02467@gmail.com>
2025-04-01 10:25:36 -0400

    change port to 8080

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
-import express, { Application, Request, Response } from 'express';
-import axios from 'axios';
-import routes from './routes/APIRoutes';
+import express, { Application, Request, Response } from "express";
+import axios from "axios";
+import routes from "./routes/APIRoutes";
 import cors from "cors";
-import { connectDB } from './config/db';
+import { connectDB } from "./config/db";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
@@ -10,18 +10,18 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 // increase body size limit to prevent "PayloadTooLargeError"
-app.use(express.json({ limit: "100mb" })); 
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 app.use(
-	cors({
-		origin: "http://localhost:3001",
-		credentials: true,
-	})
+  cors({
+    origin: "http://localhost:8080",
+    credentials: true,
+  })
 );
 
 app.use("/api", routes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
